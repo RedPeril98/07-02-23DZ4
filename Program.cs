2e99cb6704aa6eb4f35778ea60aeee564d0aaf68
Дарья <red_peril@bk.ru>
2023-02-07 15:02:09 +0300

    Задача 27

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
+/*
 int Exponentiation(int numberA, int numberB)
 {
  int result = 1;
@@ -17,3 +17,28 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 
 int exponentiation = Exponentiation(numberA, numberB);
 Console.WriteLine("Ответ: " + exponentiation);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int numberN)
+{
+ int counter = Convert.ToString(numberN).Length;
+ int advance = 0;
+ int result = 0;
+
+ for (int i = 0; i < counter; i++)
+ {
+  advance = numberN - numberN % 10;
+  result = result + (numberN - advance);
+  numberN = numberN / 10;
+ }
+ return result;
+}
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+*/
